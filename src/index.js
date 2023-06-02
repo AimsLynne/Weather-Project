4916ba70866e67018cd97d82815b95c038135a87
AimsLynne <130839244+AimsLynne@users.noreply.github.com>
2023-06-02 16:37:54 +0100

    Moved forecast HTML into JS

--- src/index.js
+++ src/index.js
@@ -104,6 +104,34 @@ function showCelsiusTemperature(event) {
   temperature.innerHTML = Math.round(celsiusTemperature);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+          `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let dateElement = document.querySelector("#date-time");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
@@ -115,6 +143,8 @@ searchForm.addEventListener("submit", submitHandle);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getcurrentLocation);
 searchCity("London");
+
+displayForecast();
 
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", showFahrenheitTemperature);
